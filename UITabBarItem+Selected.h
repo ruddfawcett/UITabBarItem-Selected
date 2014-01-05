76e15a1180790b1b33fe1275942b94f67e888214
Rex Finn <rexcfinn@gmail.com>
2014-01-05 00:35:15 -0500

    Create UITabBarItem+Selected.h

---
+++ UITabBarItem+Selected.h
@@ -0,0 +1,7 @@
+#import <UIKit/UIKit.h>
+
+@interface UITabBarItem (EasySelected)
+
+- (instancetype)initWithTitle:(NSString *)title imageName:(NSString *)image selectedImageName:(NSString *)selectedImage;
+
+@end
